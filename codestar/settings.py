ee44e8ed57c61d7535e4259eab4074456703dda1
Sarah Hill <YOUR_GITHUB_NOREPLY@users.noreply.github.com>
2026-09-01 18:21:56 +0100

    Add postgre database

--- codestar/settings.py
+++ codestar/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.path.isfile('env.py'):
+    import env
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,10 +80,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
