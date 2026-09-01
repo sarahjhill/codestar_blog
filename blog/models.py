38f5a5d994227efffe14dba71d5cda39eccf9339
Sarah Hill <YOUR_GITHUB_NOREPLY@users.noreply.github.com>
2026-09-01 22:16:13 +0100

    Add model

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
+# Create your models here.
+from django.contrib import admin
+from .models import Post
 
-
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
-    excerpt = models.TextField(blank=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+admin.site.register(Post)
