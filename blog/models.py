47257f787e4720fa0396337cb53d2614de3e36cd
Sarah Hill <YOUR_GITHUB_NOREPLY@users.noreply.github.com>
2026-09-02 17:13:26 +0100

    Add Meta ordering and __str__ to Post model

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,12 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
