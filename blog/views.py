8db007562c30ecfa2e83c8240c6fc33375c8e130
Sarah Hill <YOUR_GITHUB_NOREPLY@users.noreply.github.com>
2026-09-03 15:58:20 +0100

    Add Summernote rich text editor and admin panel customisation

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
-
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
